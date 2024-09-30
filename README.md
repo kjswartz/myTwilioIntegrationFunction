aae9611301486bc706873b0e63ea010ce6da6f4f
kjswartz <kyle.swartz.3@gmail.com>
2024-09-30 12:41:39 -0400

    setup JWT checking as a test exercise

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
+# Not For Production Use
+- Remove hard coded JWT check from index and setup JWT verifier (i.e. AWS Cognito) in API Gateway. Current JWT setup is for development testing only! 
+
 # AWS Lambda setup
 - create a new function
 - run npm install locally to setup node_modules directory
-- zip directory contents for upload to lambda function
-`zip -r ../myTwilioIntegrationFunction.zip *`
+- zip directory contents for upload to lambda function (`npm run build`)
 - upload zip file to lambda function
 - set environment variables via configuration tab
 - setup API Gateway trigger
@@ -21,6 +23,7 @@
 ```
 curl -X POST https://<url>/myTwilioIntegrationFunction \
 -H "Content-Type: application/json" \
+-H "Authorization: Bearer <token>" \
 -d '{
   "to": "<your_phone_number>",
   "body": "Ahoy World from Terminal"
--- index.js
+++ index.js
@@ -1,27 +1,49 @@
+
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
-
 const client = require('twilio')(accountSid, authToken);
 
+const jwt = require('jsonwebtoken');
+
 exports.handler = async (event, _) => {
-  const data = JSON.parse(event.body);
-  const body = data.body;
-  const to = data.to;
+  const secretKey = process.env.JWT_SECRET;
+  
+  const authHeader = event.headers?.authorization || event.headers?.Authorization;
+  if (!authHeader || !authHeader.startsWith('Bearer ')) {
+      return {
+          statusCode: 401,
+          body: JSON.stringify({ message: 'Missing or invalid Authorization header' })
+      };
+  }
+
+  const token = authHeader.split(' ')[1]; 
 
   try {
-    const message = await client.messages.create({
-      body: body,
-      from: process.env.TWILIO_NUMBER,
-      to: to,
-    });
+    jwt.verify(token, secretKey);
+
+    const data = JSON.parse(event.body);
+    const body = data.body;
+    const to = data.to;
+    try {
+      const message = await client.messages.create({
+        body: body,
+        from: process.env.TWILIO_NUMBER,
+        to: to,
+      });
+      return {
+        statusCode: 200,
+        body: JSON.stringify(message),
+      };
+    } catch (err) {
+      return {
+        statusCode: 500,
+        body: JSON.stringify(err),
+      };
+    }
+  } catch (error) {
     return {
-      statusCode: 200,
-      body: JSON.stringify(message),
-    };
-  } catch (err) {
-    return {
-      statusCode: 500,
-      body: JSON.stringify(err),
+      statusCode: 401,
+      body: JSON.stringify({ message: 'Invalid token' })
     };
   }
 }
